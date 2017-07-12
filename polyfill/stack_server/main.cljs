570a1975a8505c7ad42ed79f1c2459672490e5b3
jiyinyiyong <jiyinyiyong@gmail.com>
2017-07-12 14:13:14 +0800

    release 0.2.9 on npm to fix this issue in complation

--- polyfill/stack_server/main.cljs
+++ polyfill/stack_server/main.cljs
@@ -99,7 +99,7 @@
     (println "Port:" port)
     (println "Output:" out-folder)
     (println "Extension:" extension)
-    (println "Version: 0.2.8")
+    (println "Version: 0.2.9")
     (println (str "Edit with http://stack-editor.cirru.org/?port=" port))))
 
 (defn main! []
